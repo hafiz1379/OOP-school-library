00ec1545535457ebc7e4e9037dd850603299f4eb
hafiz1379 <hafizrasa1379@gmail.com>
2023-10-03 17:55:22 +0430

    Added Nameable base class for correct_name method

--- Lib/person.rb
+++ Lib/person.rb
@@ -1,14 +1,25 @@
-class Person
+class Nameable
+  def correct_name
+    raise NotImplementedError, "#{self.class} must implement the correct_name method"
+  end
+end
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, parent_permission: true, name: 'Unknown')
+    super()
     @id = rand(1..500)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
+  def correct_name
+    @name
+  end
+  
   def can_use_services?
     of_age? || @parent_permission
   end
