2899f2b9b7171686dbe8c8928ba49f08b770d9fe
hafiz1379 <hafizrasa1379@gmail.com>
2023-10-03 17:57:20 +0430

    Added Decorators for Person Class

--- test.rb
+++ test.rb
@@ -1,6 +1,7 @@
 require_relative 'lib/person'
 require_relative 'lib/student'
 require_relative 'lib/teacher'
+require_relative 'lib/decorator'
 
 # Create a new person
 person = Person.new(30, name: 'Omid Fakheri')
@@ -47,3 +48,16 @@ puts "Teacher age: #{teacher.age}"
 puts "Teacher ID: #{teacher.id}"
 puts "Teacher specialization: #{teacher.specialization}"
 puts "Can teacher use services?: #{teacher.can_use_services?}"
+puts 
+
+# Test Person class
+person = Person.new(22, name: 'maximilianus')
+puts "Person's correct name: #{person.correct_name}"
+
+# Test CapitalizeDecorator
+capitalized_person = CapitalizeDecorator.new(person)
+puts "Capitalized person's correct name: #{capitalized_person.correct_name}"
+
+# Test TrimmerDecorator
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts "Capitalized and trimmed person's correct name: #{capitalized_trimmed_person.correct_name}"
