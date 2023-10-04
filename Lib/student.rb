0f435d4af16a9139f9b40fa1a7d9b9bfd34ff550
hafiz1379 <hafizrasa1379@gmail.com>
2023-10-04 18:46:50 +0430

    Added classroom association to Student

--- Lib/student.rb
+++ Lib/student.rb
@@ -6,7 +6,15 @@ class Student < Person
   def initialize(age, classroom, parent_permission: true, name: 'Unknown')
     super(age, parent_permission: parent_permission, name: name)
     @classroom = classroom
+    classroom&.add_student(self)
   end
+
+  # rubocop:disable Naming/AccessorMethodName
+  # Add a method to get the classroom to which the student belongs.
+  def get_classroom
+    @classroom
+  end
+  # rubocop:enable Naming/AccessorMethodName
 
   def play_hooky
     '¯\(ツ)/¯'
