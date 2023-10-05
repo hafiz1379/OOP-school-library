b845bb57ff2babf4e9873a4625f41fece40f3548
hafiz1379 <hafizrasa1379@gmail.com>
2023-10-05 18:40:57 +0430

    Refactor Student class for improved readability and consistency

--- Lib/student.rb
+++ Lib/student.rb
@@ -1,27 +1,15 @@
 require_relative 'person'
 
+# Class representing a student
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, classroom, parent_permission: true, name: 'Unknown')
-    super(age, parent_permission: parent_permission, name: name)
-    assign_classroom(classroom)
+  def initialize(name, age, classroom, parent_permission: true)
+    super(name, age, parent_permission: parent_permission)
+    self.classroom = classroom
   end
-
-  # rubocop:disable Naming/AccessorMethodName
-  # Add a method to get the classroom to which the student belongs.
-  def get_classroom
-    @classroom
-  end
-  # rubocop:enable Naming/AccessorMethodName
 
   def play_hooky
     '¯\(ツ)/¯'
-  end
-
-  # Method to assign a classroom to the student and add the student to the classroom's list.
-  def assign_classroom(classroom)
-    @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
